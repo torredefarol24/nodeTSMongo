bf193597f6f6311f41caf4014f5592a6003ce5c2
Alex <7al@protonmail.com>
2021-11-17 23:59:31 +0300

    delete dist

--- dist/config/keys.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AppKeys = {
-    mongoDBURL: "mongodb://localhost:27017/typescriptNode",
-    mongoDBDockerURL: "mongodb://mongo:27017/typescriptNode"
-};
-exports.default = AppKeys;
-//# sourceMappingURL=keys.js.map
--- dist/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Multer = require("multer");
-let fileUploadOptions = {
-    dest: "storage"
-};
-const fileUpload = Multer(fileUploadOptions);
-exports.default = fileUpload;
-//# sourceMappingURL=multer.js.map
